914a6bac7e29fc108fc8c7be40cfef5cca0bd82b
luverktoya <maybeluver@gmail.com>
2024-02-24 02:43:37 +0200

    Update Head.tsx

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -12,8 +12,8 @@ export const RouterHead = component$(() => {
     <>
       <title>{`${head.title}`}</title>
       <meta content={`${head.title}`} property="og:title" />
-      <meta content="#54a5da" name="theme-color" />
-      <meta content="/branding/icon.png" property="og:image" />
+      <meta content="#32cd32" name="theme-color" />
+      <meta content="//luver.one/gecko.png" property="og:image" />
 
       <link rel="preload" as="font" href="/fonts/MinecraftRegular.otf" crossOrigin="anonymous" />
       <link rel="preload" as="font" href="/fonts/MinecraftBold.otf" crossOrigin="anonymous" />
@@ -21,7 +21,7 @@ export const RouterHead = component$(() => {
       <link rel="preload" as="font" href="/fonts/MinecraftBoldItalic.otf" crossOrigin="anonymous" />
 
       <link rel="canonical" href={loc.url.href} />
-      <link rel="icon" type="image/png" href="/branding/icon.svg" />
+      <link rel="icon" type="image/png" href="//luver.one/gecko.png" />
       <link rel="apple-touch-icon" href="/branding/apple-icon.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
