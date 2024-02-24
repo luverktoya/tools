0a76d0bcce28affd885ffeb5529f0f2612278170
luverktoya <maybeluver@gmail.com>
2024-02-24 02:44:44 +0200

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -19,17 +19,6 @@ export default component$(() => {
   return (
     <Nav>
       <MainNav>
-        <Dropdown name='Hosting' Icon={ServerOutline} extraClass={{ 'hidden sm:flex': true }}>
-          <NavButton type="external" href="https://panel.birdflop.com/">
-            Panel
-          </NavButton>
-          <NavButton type="external" href="https://client.birdflop.com/">
-            Billing
-          </NavButton>
-          <NavButton href="/node-stats">
-            Node Stats
-          </NavButton>
-        </Dropdown>
         <Dropdown name="Resources" Icon={CubeOutline} extraClass={{ 'hidden sm:flex': true }}>
           <NavButton href="/resources/rgb">
             {t('nav.hexGradient@@Hex Gradients')}
@@ -47,22 +36,12 @@ export default component$(() => {
             {t('nav.more@@More Resources')}
           </NavButton>
         </Dropdown>
-        <LangPicker extraClass={{ 'hidden': !location.url.pathname.includes('resources') }} />
         <SocialButtons />
         <NavButton type="div" icon title="Menu" onClick$={() => { store.mobilemenu = !store.mobilemenu; }} extraClass={{ 'flex sm:hidden fill-current hover:fill-white': true }}>
           <Menu width="24" />
         </NavButton>
       </MainNav>
       <MobileNav store={store}>
-        <NavButton type="external" href="https://panel.birdflop.com/">
-          Panel
-        </NavButton>
-        <NavButton type="external" href="https://client.birdflop.com/">
-          Billing
-        </NavButton>
-        <NavButton href="/node-stats">
-          Node Stats
-        </NavButton>
         <NavButton store={store} href="/resources/rgb">
           {t('nav.hexGradient@@Hex Gradients')}
         </NavButton>
@@ -106,9 +85,8 @@ export const Brand = component$(() => {
   const location = useLocation();
   return (
     <div class="flex items-center justify-start">
-      <Link href="/" class="transition ease-in-out text-gray-100 hover:bg-blue-700/20 hover:text-white drop-shadow-xl px-3 pb-3 pt-3 rounded-lg text-lg flex tracking-wider items-center">
-        <LogoBirdflop width={32} fillGradient={['#54daf4', '#545eb6']} />
-        <span class="ml-3 font-bold">Birdflop</span>
+      <Link href="/" class="transition ease-in-out text-gray-100 hover:bg-neutral-700/20 hover:text-white drop-shadow-xl px-3 pb-3 pt-3 rounded-lg text-lg flex tracking-wider items-center">
+        <img src="//luver.one/48.png" />
         <div class={{
           'transition-all pl-2': true,
           '-ml-6 opacity-0': !location.isNavigating,
@@ -122,7 +100,7 @@ export const Brand = component$(() => {
 
 export const MainNav = component$(() => {
   return (
-    <div class={'bg-blue-700/20 py-2'}>
+    <div class={'bg-neutral-700/20 py-2'}>
       <div class={'mx-auto relative flex items-center justify-between max-w-7xl px-2'}>
         <Brand />
         <div class="flex flex-1 items-center justify-end">
@@ -138,7 +116,7 @@ export const MainNav = component$(() => {
 export const MobileNav = component$(({ store }: any) => {
   return (
     <div id="mobile-menu" class={{
-      'gap-2 px-3 flex flex-col sm:hidden transition-all duration-300 bg-blue-700/20 ': true,
+      'gap-2 px-3 flex flex-col sm:hidden transition-all duration-300 bg-neutral-700/20 ': true,
       'opacity-100 max-h-screen pt-2 pb-8': store.mobilemenu,
       'opacity-0 max-h-0 py-0 pointer-events-none': !store.mobilemenu,
     }}>
@@ -149,7 +127,7 @@ export const MobileNav = component$(({ store }: any) => {
 
 export const NavButton = component$(({ href, title, icon, type, extraClass, style, store, onClick$ }: any) => {
   const _class = {
-    'group transition ease-in-out hover:bg-blue-700/20 hover:text-white py-3 rounded-lg items-center cursor-pointer': true,
+    'group transition ease-in-out hover:bg-neutral-700/20 hover:text-white py-3 rounded-lg items-center cursor-pointer': true,
     'text-3xl px-3': icon,
     'px-4 flex gap-3': !icon,
     ...extraClass,
@@ -177,7 +155,7 @@ export const NavButton = component$(({ href, title, icon, type, extraClass, styl
 export const Dropdown = component$(({ name, Icon, extraClass }: any) => {
   return (
     <div class={{
-      'transition ease-in-out gap-3 hover:bg-blue-700/20 hover:text-white drop-shadow-2xl group rounded-lg items-center': true,
+      'transition ease-in-out gap-3 hover:bg-neutral-700/20 hover:text-white drop-shadow-2xl group rounded-lg items-center': true,
       ...extraClass,
     }}>
       <div class="px-4 py-2 flex gap-2.5 items-center">
@@ -186,7 +164,7 @@ export const Dropdown = component$(({ name, Icon, extraClass }: any) => {
         <ChevronDown width="16" class="transform group-hover:-rotate-180 transition ease-in-out" />
       </div>
       <div class="absolute top-8 z-10 hidden group-hover:flex pt-8 text-base">
-        <div class="bg-gray-900 border border-gray-800 rounded-xl px-3 py-4 flex flex-col gap-2 font-medium whitespace-nowrap overflow-y-auto max-h-[calc(100svh-128px)]">
+        <div class="bg-neutral-900 border border-neutral-800 rounded-xl px-3 py-4 flex flex-col gap-2 font-medium whitespace-nowrap overflow-y-auto max-h-[calc(100svh-128px)]">
           <Slot />
         </div>
       </div>
@@ -204,14 +182,14 @@ export const LangPicker = component$(({ extraClass }: any) => {
 
   return (
     <div class={{
-      'cursor-pointer transition ease-in-out flex hover:bg-blue-700/20 hover:text-white drop-shadow-2xl group rounded-lg text-3xl items-center gap-4': true,
+      'cursor-pointer transition ease-in-out flex hover:bg-neutral-700/20 hover:text-white drop-shadow-2xl group rounded-lg text-3xl items-center gap-4': true,
       ...extraClass,
     }}>
       <div class="p-3">
         <GlobeOutline width="24" class="transform group-hover:rotate-180 group-hover:text-white transition ease-in-out" />
       </div>
       <div class={'absolute top-8 z-10 hidden group-hover:flex pt-5 text-base right-0'}>
-        <div class="bg-gray-900 border border-gray-800 rounded-xl px-3 py-4 flex flex-col gap-2 font-medium whitespace-nowrap overflow-y-auto max-h-[calc(100svh-128px)]">
+        <div class="bg-neutral-900 border border-neutral-800 rounded-xl px-3 py-4 flex flex-col gap-2 font-medium whitespace-nowrap overflow-y-auto max-h-[calc(100svh-128px)]">
           {config.supportedLocales.map(value => (
             <NavButton type="div" key={value.lang} onClick$={async () => await changeLocale$(value)}>
               {languages[value.lang as keyof typeof languages]}
@@ -227,9 +205,6 @@ export const SocialButtons = component$(() => {
   return <>
     <NavButton type="external" icon href="https://github.com/birdflop/web" title="GitHub" extraClass={{ 'flex fill-current hover:fill-white': true }}>
       <LogoGithub width="24" />
-    </NavButton>
-    <NavButton type="external" icon href="https://discord.gg/nmgtX5z" title="Discord" extraClass={{ 'flex fill-current hover:fill-white': true }}>
-      <LogoDiscord width="24" />
     </NavButton>
   </>;
 });
